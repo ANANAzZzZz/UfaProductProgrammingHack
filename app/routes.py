9afc360214d12dc4130b2c5e26bbe767d87afecb
Malyshev_Andrei <andrewmlshv@mail.ru>
2024-05-16 11:24:45 +0300

    патч габена

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,35 @@
-import flask
+from flask import request, jsonify
 from app import app, db
+from werkzeug.security import generate_password_hash
 
 
 @app.route('/')
 def index():
     return 'Index Page'
+
+
+@app.route("/registration", methods=["POST"])
+def registration():
+    data = request.get_json()
+    if not data:
+        return jsonify("Missing data"), 400
+
+    username = data.get('username')
+    mail = data.get('mail')
+    password = data.get('password')
+    role = data.get('role')
+    photo = data.get('photo')
+    user = db.find_user_by_email(mail)
+    if user:
+        return jsonify("A user with such mail already exists")
+    user = db.find_user_by_username(username)
+    if user:
+        return jsonify("A user with such name already exists")
+    user = db.add_user(username,  generate_password_hash(password), mail, role, photo)
+    if user:
+        return jsonify(message='Вы успешно добавлены'), 200
+    else:
+        return jsonify(message='Неверные учетные данные'), 401
 
 
 @app.route('/news')
--- db/DBInterface.py
+++ db/DBInterface.py
@@ -12,12 +12,53 @@ class DBInterface:
                              dbname=Config.DB_NAME) as con:
 
             cur = con.cursor()
-
             cur.execute('SELECT * FROM News')
-
             result = cur.fetchall()
-
             if not result:
                 print('News not found')
+                return None
+            return result
+
+    @staticmethod
+    def find_user_by_email(email):
+        with psycopg.connect(host=Config.DB_SERVER,
+                             user=Config.DB_USER,
+                             password=Config.DB_PASSWORD,
+                             dbname=Config.DB_NAME) as con:
+
+            cur = con.cursor()
+            cur.execute("SELECT * FROM \"User\" WHERE email = %s", (email,))
+            result = cur.fetchone()
+            if not result:
+                return None
+            return result
+
+    @staticmethod
+    def add_user(username, password, email, role, photo):
+        with psycopg.connect(host=Config.DB_SERVER,
+                             user=Config.DB_USER,
+                             password=Config.DB_PASSWORD,
+                             dbname=Config.DB_NAME) as con:
+
+            cur = con.cursor()
+            cur.execute("INSERT INTO \"User\" (username, password, email, role, photo) VALUES (%s, %s, %s, %s, %s) RETURNING username",
+                        (username, password, email, role, photo))
+            result = cur.fetchone()
+            con.commit()
+            if not result:
+                return None
+            return result
+
+    @staticmethod
+    def find_user_by_username(username):
+        with psycopg.connect(host=Config.DB_SERVER,
+                             user=Config.DB_USER,
+                             password=Config.DB_PASSWORD,
+                             dbname=Config.DB_NAME) as con:
+
+            cur = con.cursor()
+            cur.execute("SELECT * FROM \"User\" WHERE username = %s", (username,))
+            result = cur.fetchone()
+            if not result:
                 return None
             return result
