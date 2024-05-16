71e4620c91f657539e69d17c40005ec70a18f5f9
ANANAzZzZz <vladscooltrol@gmail.com>
2024-05-16 09:57:14 +0300

    add news endpoint

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,19 @@ def index():
 
 
 @app.route('/news')
-def hello():
-    print(db.getNews())
-    return 'Hello, World'
+def retNews():
+    news = db.getNews()
+
+    if not news:
+        return []  # "No avaliable tracks"
+
+    newsList = []
+    for el in news:
+        dict = {
+            'id': el[0],
+            'userId': el[1],
+            'dateStamp': el[2],
+            'description': el[3]
+        }
+        newsList.append(dict)
+    return newsList
